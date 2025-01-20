d26a68152ae72380e5612a939a228ce0564735aa
Stephen <hellostephenwm@gmail.com>
2025-01-20 18:34:14 -0500

    feat: enable promise polyfill plugin to add promise support for older browsers

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import "regenerator-runtime/runtime.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import PromisePolyfill from "promise-polyfill";
+
+if (!window.Promise) {
+	window.Promise = PromisePolyfill;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

aadf4a650d7004992b7c2433650a1f92aa26d1e6
Stephen <hellostephenwm@gmail.com>
2025-01-20 18:09:19 -0500

    chore: custom hook to fetch notes from backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./index.css";
+
+/**
+ * Custom hook to fetch notes from backend
+ * @param {*} url
+ * @returns Array of notes
+ */
+const useNotes = (url) => {
+	const [notes, setNotes] = useState([]);
+
+	useEffect(() => {
+		axios.get(url).then((response) => {
+			setNotes(response.data);
+		});
+	}, [url]);
+
+	return notes;
+};
 
 const App = () => {
 	const [counter, setCounter] = useState(0);
 	const [values, setValues] = useState([]);
+
+	const url = "https://notes2023.fly.dev/api/notes";
+	const notes = useNotes(url);
 
 	const handleClick = () => {
 		setCounter((prev) => prev + 1);
@@ -14,6 +35,9 @@ const App = () => {
 		<div className="container">
 			hello webpack {counter} clicks{" "}
 			<button onClick={handleClick}>press</button>
+			<div>
+				{notes.length} notes on server {url}
+			</div>
 		</div>
 	);
 };
